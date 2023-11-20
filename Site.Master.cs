cbbdc184031bb002c462c330b49e633073a0296f
austi <austi@GWTN156-7>
2023-11-20 15:20:23 -0600

    code for insnstructor

--- Site.Master.cs
+++ Site.Master.cs
@@ -11,7 +11,7 @@ namespace Karate
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
--- mywork/instructor.aspx.cs
+++ mywork/instructor.aspx.cs
@@ -9,9 +9,32 @@ namespace Karate.mywork
 {
     public partial class instructor : System.Web.UI.Page
     {
+
+        string firstname;
+        string lastname;
+        
+        string conn;
+        
+       
+
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\austi\\Source\\Repos\\vanberkom\\Karate2\\App_Data\\KarateSchool(1) (1).mdf\";Integrated Security=True;Connect Timeout=30";
+            dbcon = new KarateDBDataContext(conn);
+
+
+            var instructors = from net in dbcon.NetUsers
+                              join instructor in dbcon.Instructors on net.UserID equals instructor.InstructorID
+                              select new { instructor.InstructorFirstName, instructor.InstructorLastName };
+
+            GridView1.DataSource = instructors;
+            GridView1.DataBind();
+
 
         }
+
+
     }
 }
