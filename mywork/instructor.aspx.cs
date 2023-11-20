f5e8537e43d3be3ff898f1bd6ae84a5328444ee4
luke.vanberkom <luke.vanberkom@SCL-QUBB0200-03>
2023-11-20 15:50:17 -0600

    Changeds

--- mywork/instructor.aspx.cs
+++ mywork/instructor.aspx.cs
@@ -14,33 +14,12 @@ namespace Karate.mywork
         string lastname;
         
         string conn;
-<<<<<<< HEAD
         KarateDBDataContext dbcon;
-=======
-        string dbcon;
->>>>>>> 3cf858d5b528191d0dcf50f590644cf4b370b062
-       
-
-
 
         protected void Page_Load(object sender, EventArgs e)
         {
-<<<<<<< HEAD
             conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\luke.vanberkom\\Source\\Repos\\vanberkom\\Karate2\\App_Data\\KarateSchool(1) (1).mdf\";Integrated Security=True;Connect Timeout=30";
             dbcon = new KarateDBDataContext(conn);
-=======
-            conn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\austi\\Source\\Repos\\vanberkom\\Karate2\\App_Data\\KarateSchool(1) (1).mdf\";Integrated Security=True;Connect Timeout=30";
-            dbcon = new KarateDBDataContext(conn);
-
-
-            var instructors = from net in dbcon.NetUsers
-                              join instructor in dbcon.Instructors on net.UserID equals instructor.InstructorID
-                              select new { instructor.InstructorFirstName, instructor.InstructorLastName };
-
-            GridView1.DataSource = instructors;
-            GridView1.DataBind();
-
->>>>>>> 3cf858d5b528191d0dcf50f590644cf4b370b062
 
 
             var instructors = from net in dbcon.NetUsers
